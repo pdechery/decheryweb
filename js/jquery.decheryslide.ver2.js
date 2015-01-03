76ab43849fde31f62421111d736a3996c5482435
Pierre Dechery <pierre@decheryweb.com.br>
2015-01-03 14:40:26 -0200

    Nova versão plugin Slideshow
    Novo código JS utilizando objetos e prototype.

--- js/jquery.decheryslide.ver2.js
+++ js/jquery.decheryslide.ver2.js
@@ -1,67 +1,64 @@
 // PLUGIN SLIDESHOW DECHERYWEB
-// Código de acordo com pattern de desenvolvimento de plugin para jquery
-// encontrado em: http://debuggable.com/posts/how-to-write-jquery-plugins:4f72ab2e-7310-4a74-817a-0a04cbdd56cb
 // Início desenvolvimento: 2/1/2015
+
+// Código de acordo com pattern de desenvolvimento de plugin para jquery
+// Referência: http://debuggable.com/posts/how-to-write-jquery-plugins:4f72ab2e-7310-4a74-817a-0a04cbdd56cb
 
 (function($, doc, win) {
 
-	// função construtora que será instanciada na invocação do plugin mais tarde
-	// esta função irá declarar variáveis para todos objetos jquery usados no plugin
-	// assim como os parâmetros passados a cada invocação.
-	function decherySlide(el, opts) {
+	// função construtora que será instanciada na invocação do plugin
+	// cria propriedades para os elementos DOM usados no plugin e os parâmetros 
+	function DecherySlide(el, opts) {
 
-		// opções default do plugin
+		// opções default
 		this.defaults = {
 			slideWidth: 440,
 			slideHeight: 510,
 			currentPosition: 0,
 			navigation: true,
 			pagination: false,
-			auto: true,
+			auto: false,
 			speed: 500,
-			el: '.proj img',
-			prtel: '.area',
+			imgs: '.proj img', // classe das imagens da galeria
+			parentEl: '.area', // container
 			prevIcon: '<i class="fa fa-angle-left fa-lg"></i>',
 			nextIcon: '<i class="fa fa-angle-right fa-lg"></i>'
 		};
 
-		// criando objeto jquery com o elemento no qual o plugin será aplicado
-		this.$el = $(el);
+		this.el = el; // projeto onde vai ser aplicado o plugin
 
-		this.el = el;
-
-		// objeto que mescla opções default com as do usuário
-		this.opts = $.extend(this.defaults, opts);
+		this.opts = $.extend(this.defaults, opts); // parâmetros
 		
-		// elementos DOM/objetos jQuery
-		
-		this.$slideEl = $(this.opts.el, el); // elemento contendo o html aonde será aplicada a animação
+		this.$slideImgs = $(this.opts.imgs, el); // imagens do projeto
 
-		this.$navEl = $(this.opts.prtel, el); // elemento que contém a "área total" usada na lógica da navegação
+		this.$prtEl = $(this.opts.parentEl, el); // parent das imagens do projeto
 
-		this.total = this.$slideEl.length; // número total de elementos que vão virar slides
+		this.total = this.$slideImgs.length; // total de slides
 		
-		this.steps = this.$slideEl.length - 1; // número total menos 1
-
+		this.steps = this.$slideImgs.length - 1;
 		
-		// iniciando o plugin com o método 'init', declarado logo abaixo, com o prototype.
+		// iniciando o plugin com o método 'init', declarado logo abaixo
 		this.init();
 
 	}
 
-	// lógica do plugin
-	decherySlide.prototype.init = function() {
+	DecherySlide.prototype.init = function() {
 
 		var self = this;
 
-		// criando um wrapper e definindo seus atributos (width)
-		this.$slideEl.wrapAll('<div class="slidesHolder"></div>'); // html wrapper dos slides
+		this.$slideImgs.wrapAll('<div class="slidesHolder"></div>'); // html wrapper dos slides
 		
 		this.$wrapper = $('.slidesHolder', this.el);
 		
 		this.$wrapper.css('width', this.opts.slideWidth * this.total);
 
-		// auto execução (opcional)
+		if(this.opts.pagination) {
+			this.$pagination = $("<ul>", {"id": "slidenav", "class": "clearfix"}).prependTo($(this.$prtEl, this.el)); // wrapper paginação bullets
+			this.setPagination();
+		}
+
+		if(this.opts.navigation) this.setNavigation();
+
 		if(this.opts.auto) {
 			var roda = function() {
 				self.moveSlide('next');
@@ -72,7 +69,7 @@
 	}
 
 	// a função que anima os slides
-	decherySlide.prototype.moveSlide = function(dir) {
+	DecherySlide.prototype.moveSlide = function(dir) {
 		
 		var ponteiro = this.opts.currentPosition;
 		
@@ -89,14 +86,83 @@
 		
 		this.$wrapper.animate({'marginLeft' : this.opts.slideWidth*(-this.opts.currentPosition)});
 		
-		// if(this.opts.pagination) setActive(this.opts.currentPosition); atualiza bullet da paginação
+		if(this.opts.pagination) this.setActive(this.opts.currentPosition); // atualiza bullet da paginação
 	
 	}
+
+	DecherySlide.prototype.setActive = function(number) {
+		
+		if(this.opts.pagination) {
+			var current = number > -1 ? number : 0;
+			$(".active", this.$pagination).removeClass("active");
+			$("li:eq(" + current + ") a", this.$pagination).addClass("active");
+		}
+
+		if(number === this.steps) {
+			$('a.nextImg',this.el).addClass('off');
+			$('a.prevImg',this.el).removeClass('off');
+		} else if(number < this.steps && number !== 0) {
+			$('a.nextImg',this.el).removeClass('off');
+			$('a.prevImg',this.el).removeClass('off');
+		} else if(number === 0) {
+			$('a.nextImg',this.el).removeClass('off');
+			$('a.prevImg',this.el).addClass('off');
+		}
+
+	}
+
+	DecherySlide.prototype.setNavigation = function() {
+
+		var $navWrap = $('<div class="navigation"></div>').prependTo($(this.$prtEl, this.el)); // html wrapper da navegação 
+
+		var html = ' <a class="prevImg">'+ this.opts.prevIcon +'</a>';
+		html += ' <a class="nextImg">'+ this.opts.nextIcon +'</a>';
+		
+		$navWrap.prepend(html);
+
+		var self = this;
+
+		$('a.prevImg', this.el).click(function() {		
+			self.moveSlide('prev');
+		});
+		$('a.nextImg', this.el).click(function() {
+			self.moveSlide('next');		
+		})
+
+	}
+
+	DecherySlide.prototype.setPagination = function() {
+
+		var self = this;
+
+		$.each(new Array(this.total), function(i) {
+
+			var paginationItem, paginationLink;
+			
+			paginationItem = $("<li>").appendTo(self.$pagination);
+			
+			paginationLink = $("<a>", {
+				href: "#",
+				"item": i,
+				"class": i == 0 ? 'active' : ''
+			}).appendTo(paginationItem);
+
+			return paginationLink.click(function(e) {
+				var currPos;
+				e.preventDefault();
+				currPos = $(e.currentTarget).attr("item");
+				self.opts.currentPosition = parseInt(currPos); // altera o currentPosition, e transforma currPos em número inteiro
+				self.moveSlide(currPos);
+			});
+		
+		})
+	}
+		
 
 	$.fn.decherySlide = function(opts) {
 		
 		return this.each(function() {
-			new decherySlide(this, opts);
+			new DecherySlide(this, opts);
 		});
 	
 	};
