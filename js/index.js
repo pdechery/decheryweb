6a0acb889afa0da95fb6b38e487c141b61c0e856
Pierre Dechery <pierre.dechery@gmail.com>
2022-04-18 17:14:17 -0300

    - Versão em inglês

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 const btn = document.querySelector('.btn a');
 const contentArea = document.querySelector('#content');
+const en = contentArea.querySelector('#en');
+const pt_br = contentArea.querySelector('#pt_br');
+const lang_switch = document.querySelector('#lang_switch');
+const aside = document.querySelector('aside');
+const icones = document.querySelector('ul#icones');
+
+let is_english = 0;
 
 btn.addEventListener('click', (el) => {
 
@@ -9,4 +16,8 @@ btn.addEventListener('click', (el) => {
 		behavior: 'smooth'
 	});
 
-})
+	// setInterval(function(){
+	// 	lang_switch.className = 'show';
+	// }, 800);
+
+});
